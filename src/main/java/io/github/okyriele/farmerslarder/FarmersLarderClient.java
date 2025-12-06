75151bb57f291e7d49a099fbfd198e2fe6afbf98
joymersmann <61635582+joymersmann@users.noreply.github.com>
2025-12-06 10:11:30 -0500

    Initial commit

---
+++ src/main/java/io/github/okyriele/farmerslarder/FarmersLarderClient.java
@@ -0,0 +1,31 @@
+package io.github.okyriele.farmerslarder;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = FarmersLarder.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = FarmersLarder.MODID, value = Dist.CLIENT)
+public class FarmersLarderClient {
+    public FarmersLarderClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        FarmersLarder.LOGGER.info("HELLO FROM CLIENT SETUP");
+        FarmersLarder.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
